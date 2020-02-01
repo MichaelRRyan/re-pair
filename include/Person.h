a78e8a87a67cf9a667392797470f50fb08f5bb99
Michael Rainsford Ryan <c00239510@itcarlow.ie>
2020-02-01 11:29:50 +0000

    Added basic Person implementation

--- include/Person.h
+++ include/Person.h
@@ -4,9 +4,17 @@
 /// @Author Michael R Ryan
 /// @Date 01/02/2020
 
-class Person
-{
+#include <SFML/Graphics.hpp>
 
+class Person : public sf::Drawable
+{
+public:
+	Person();
+
+	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+
+private:
+	sf::RectangleShape m_boundingBox;
 };
 
 #endif // !NPC_H
--- src/Person.cpp
+++ src/Person.cpp
@@ -1,1 +1,14 @@
 #include "../include/Person.h"
+
+Person::Person()
+{
+    m_boundingBox.setFillColor(sf::Color{ static_cast<sf::Uint8>(rand() % 255),static_cast<sf::Uint8>(rand() % 255),static_cast<sf::Uint8>(rand() % 255) });
+    m_boundingBox.setPosition(400.0f, 200.0f);
+    m_boundingBox.setSize({ 30.0f, 60.0f });
+}
+
+void Person::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
+    // You can draw other high-level objects
+    target.draw(m_boundingBox, states);
+}
